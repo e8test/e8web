11145be40b2294a5f8658ad352235f1937e3c822
Vitalik Buterin <VitalikZhou@gmail.com>
2022-01-04 21:36:35 +0800

    modify router address

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ const config: IConfig = {
       chainName: 'Binance Smart Chain Testnet',
       rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       nftAddr: '0xB6B83EFa0ac59d085562934a873d784D9BA2949e',
-      routerAddr: '0xfE16a7903153FD1B3Aa71BDa993BC5Ad7EAdb482',
+      routerAddr: '0x0752F13a3c8C6877088A10B01Edc2250b6C42a3f',
       tokenAddr: '0xd7765F498B77d9C8F050BBD78620c33523E703b4'
     },
     56: {
