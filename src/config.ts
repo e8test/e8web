fa5488902e102c4d1fb366e91e3077384452c6d5
Vitalik Buterin <VitalikZhou@gmail.com>
2022-03-04 16:41:41 +0800

    new router address

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const CONFIGS: IConfig = {
       rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
       ethscan: 'https://testnet.bscscan.com',
       nftAddr: '0xB6B83EFa0ac59d085562934a873d784D9BA2949e',
-      routerAddr: '0xc68Ef8dBdd29Db37bee859Ac9fFBB8EFB0022030',
+      routerAddr: '0x6FfBbe2291D6D51EBeF70E79FA537dE2230BaAaa',
       tokenAddr: '0x50c0b7228dd8Aa6B28B3848Ba5fe762fFeE84AA3',
       logAddr: '0x855fddac7e02437f0a70fb21886d84e258ce331cce8154890dd9d805089e60c8'
     }
