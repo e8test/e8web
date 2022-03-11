6bf9b6d9d3fc2411a4a536f35f5f795e419a632b
Vitalik Buterin <VitalikZhou@gmail.com>
2022-03-11 12:00:54 +0800

    getStatus返回上次申请估价时间

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const CONFIGS: IConfig = {
       ethscan: 'https://testnet.bscscan.com',
       tokenName: 'E8INDEX',
       nftAddr: '0xB6B83EFa0ac59d085562934a873d784D9BA2949e',
-      routerAddr: '0x0CF75A37c5691E2773CD40EC819Fe0d50D14ed66',
+      routerAddr: '0xaB3eDE6D0772bE651E16D444e0F4e13a581Ae150',
       tokenAddr: '0xf8b2BA003C47c997e3ffa4ebb94cf17183c9e190',
       logAddr: '0x855fddac7e02437f0a70fb21886d84e258ce331cce8154890dd9d805089e60c8'
     }
