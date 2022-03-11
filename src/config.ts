38f29c71619b0c3aeb46d89a1d54ca6e44b6f5b5
Vitalik Buterin <VitalikZhou@gmail.com>
2022-03-11 16:16:58 +0800

    拍卖合约

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,8 @@ const CONFIGS: IConfig = {
       ethscan: 'https://testnet.bscscan.com',
       tokenName: 'E8INDEX',
       nftAddr: '0xB6B83EFa0ac59d085562934a873d784D9BA2949e',
-      routerAddr: '0xaB3eDE6D0772bE651E16D444e0F4e13a581Ae150',
+      routerAddr: '0x3a2157E493F9A247345407013eCFF981f877aD1A',
+      auctions: '0xd5765a40480d496cd08Caa17c5314EC96Fab4bA6',
       tokenAddr: '0xf8b2BA003C47c997e3ffa4ebb94cf17183c9e190',
       logAddr: '0x855fddac7e02437f0a70fb21886d84e258ce331cce8154890dd9d805089e60c8'
     }
