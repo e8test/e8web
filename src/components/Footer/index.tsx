aa7964f4519214eaf48e32bb41745592ef241686
jssjss <ss.chiang@foxmail.com>
2022-06-15 00:40:43 +0800

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,6 @@ import styles from './style.module.scss'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.tip}>This quote is valid for 15 minutes.</div>
       <Divider />
       <div className={styles.copyright}>
         Copyright 2022 Index & Liquidity System All Rights Reserved
